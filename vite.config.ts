56b82bd5322691a6541288f63c6926db0fc173b4
chaowen44-cloud <chaowen44@gmail.com>
2026-06-13 02:00:27 +0800

    Set base path for LINE-Project-Workplace

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/LINE-Project-Workplace/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
